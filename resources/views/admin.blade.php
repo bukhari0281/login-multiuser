9cfab2b491366c395c40f1f8a25904a2ef937bd5
Al Bukhari Bin Riedho <bukhari0281@gmail.com>
2023-10-14 01:26:48 +0700

    .

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,18 +1,13 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <div class="container">
+<h1>Admin Dashboard</h1>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
+        <div class="titlebar">
+          <h1>Blog list</h1>
         </div>
-    </div>
+        <hr>
+        <p>The Blog 1 - {{ $post }}</p>
+      </div>
 </x-app-layout>
 
 
-<h1>Admin Dashboard</h1>
